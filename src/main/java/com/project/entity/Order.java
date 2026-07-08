0eb89c61ab46a348dff4fb8f12ea5e752df9585a
ASHUtosh <ashutoshkumar14331@gmail.com>
2026-07-08 17:55:03 +0530

    create Order Entity and add some fieled

--- src/main/java/com/project/entity/Order.java
+++ src/main/java/com/project/entity/Order.java
@@ -1,4 +1,21 @@
 package com.project.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order{
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
